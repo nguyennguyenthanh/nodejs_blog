a12b6e9625db713f6049f88b533bf14b8210e437
nguyennguyenthanh <thanhnguyennguyen1228@gmail.com>
2024-02-26 11:56:12 +0700

    Remove public/css folder

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 //tạo express trong node-moudle
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 //gọi hàm ra để chạy source
 const app = express();
 //tùy chọn port được
 const port = 3000;
-
+//static file
+app.use(express.static(path.join(__dirname, 'public')));
 //https logger
 app.use(morgan('combined'));
-
 //teamplate engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-// app.set('views', path.join(__dirname, 'views'));
-console.log(__dirname);
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 //route 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/news', (req, res) => {
+  res.render('news');
+});
 // Ip: 12.0.0.1 - localhost
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
